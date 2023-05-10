7de91017fe05ca9268a1864de53037317e4d7d4a
meomeocoj <tminh1103@gmail.com>
2023-05-10 14:31:42 +0700

    init: project

---
+++ contracts/manager-license/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use manager_license::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
