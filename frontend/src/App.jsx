6ba4e950658846554df6620c6edcf9f49108a9aa
trantheanh.ktmt <trantheanh.ktmt@gmail.com>
2023-05-13 15:36:33 +0700

    chore: temp commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
 
+import LandingPage from "./components/LandingPage";
+
 const App = () => {
   return (
     <AppLayout>
       <Routes>
-        <Route path="/" element={<></>} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </AppLayout>
   );
---
+++ frontend/src/components/LandingPage/Sections/MainSection/MainSection.jsx
@@ -0,0 +1,3 @@
+export const MainSection = () => {
+  return <div>MainSection</div>;
+};
