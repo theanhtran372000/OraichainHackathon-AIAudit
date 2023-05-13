86045850dd9aec27d4c7036f4b0930c6a6a9a418
trantheanh.ktmt <trantheanh.ktmt@gmail.com>
2023-05-13 13:58:47 +0700

    feat: add init code

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,10 @@
+import classNames from "classnames/bind";
+import style from "./NavBar.module.sass";
+
+const cx = classNames.bind(style);
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return <div className={cx("header", "navbar")}>NavBar</div>;
 };
 
 export default NavBar;
