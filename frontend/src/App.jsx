ad7a5cddcfdfad7892d9ff21245fb49a5d262d33
Trần Đình Nguyên <nguyentran9122000@gmail.com>
2023-05-13 13:59:44 +0700

    Merge branch 'fe' of https://github.com/theanhtran372000/OraichainHackathon-AIAudit into fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
-import NavBar from "./components/NavBar/NavBar";
+// import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <AppLayout>
       <Routes>
-        <Route path="/" element={<NavBar />} />
+        <Route path="/" />
       </Routes>
     </AppLayout>
   );
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,44 @@
+import classNames from "classnames/bind";
+import style from "./Footer.module.sass";
+import { Row, Col } from "antd";
+const cx = classNames.bind(style);
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <>
+      <Row className={cx("footer")}>
+        <Col className={cx("col-1")}>
+          <Row>
+            <Col>Phone number</Col>
+            <Col>0123-323-1331</Col>
+          </Row>
+          <Row>
+            <Col>Phone number</Col>
+            <Col>0123-323-1331</Col>
+          </Row>
+          <Row>
+            <Col>Phone number</Col>
+            <Col>0123-323-1331</Col>
+          </Row>
+        </Col>
 
-export default Footer
+        <Col className={cx("col-1")}>
+          <Row>
+            <Col>Phone number</Col>
+            <Col>0123-323-1331</Col>
+          </Row>
+          <Row>
+            <Col>Phone number</Col>
+            <Col>0123-323-1331</Col>
+          </Row>
+          <Row>
+            <Col>Phone number</Col>
+            <Col>0123-323-1331</Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Footer;
