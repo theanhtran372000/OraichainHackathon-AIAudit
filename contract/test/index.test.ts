745957608404256f8731442fd2d038098f340f5d
meomeocoj <tminh1103@gmail.com>
2023-05-13 08:42:46 +0700

    add: deploy test

--- contract/test/index.test.ts
+++ contract/test/index.test.ts
@@ -2,51 +2,77 @@ import { SimulateCosmWasmClient } from "@terran-one/cw-simulate";
 import { resolve } from "path";
 import { InstantiateMsg as ManagerInstantiateMsg } from "../artifacts/contracts/ManagerLicense.types";
 import { InstantiateMsg as AggregatorInstantiateMsg } from "../artifacts/contracts/Aggregator.types";
+import { AggregatorClient } from "../artifacts/contracts/Aggregator.client";
+import { ManagerLicenseClient } from "../artifacts/contracts/ManagerLicense.client";
 
-const prefix = "orai";
-const chainId = "Oraichain";
-const sender = `${prefix}2hgm0p7khfk85zpz5v0j8wnej3a90w709vhkdfu`;
-const funds = [];
+describe("full-flow", () => {
+  let managerAddress, aggregatorAddress;
+  let ManagerContract: ManagerLicenseClient;
+  let AggregatorContract: AggregatorClient;
 
-// instantiate the contract
-const main = async function() {
-  const client = new SimulateCosmWasmClient({
-    chainId,
-    bech32Prefix: prefix,
+  const prefix = "orai";
+  const chainId = "Oraichain";
+  const sender = `${prefix}1nw5dvqa6j649aaf2hdgqfqa47lpmv77yyu5ulw`;
+  const hosts = [
+    "orai13yg9xfceku6syswujw8dhgxe8wlqvf7c09xcvn",
+    "orai12mmyfm0pxr9s2gn9rlc7xx8hfscugv0tmtqcvv",
+    "orai16ahu3tsajmd84nnlk6p8rp8tzssctjlzcjcgcn",
+    "orai1jzsqvhw8az9jms5pgw5tv2r2fw2ygmggetha3j",
+    "orai1zkytv5pfy5dl7gdgpw80z2ukmj80cvx2mwgec6",
+  ];
+
+  beforeAll(async () => {
+    const client = new SimulateCosmWasmClient({
+      zkFeatures: true,
+      chainId,
+      bech32Prefix: prefix,
+    });
+
+    // import the wasm bytecode
+    let instantiate: ManagerInstantiateMsg = {
+      amount_reward: "10000",
+      owner: sender,
+      register_fee: "10000",
+      thresh_hold_for: 40,
+    };
+
+    ({ contractAddress: managerAddress } = await client.deploy(
+      sender,
+      resolve(__dirname, "../artifacts/manager_license-aarch64.wasm"),
+      instantiate,
+      "manager",
+      "auto"
+    ));
+
+    let instantiate_msg: AggregatorInstantiateMsg = {
+      manager: managerAddress,
+    };
+
+    ({ contractAddress: aggregatorAddress } = await client.deploy(
+      sender,
+      resolve(__dirname, "../artifacts/aggregator-aarch64.wasm"),
+      instantiate_msg,
+      "manager",
+      "auto"
+    ));
+
+    AggregatorContract = new AggregatorClient(
+      client,
+      sender,
+      aggregatorAddress
+    );
+
+    ManagerContract = new ManagerLicenseClient(client, sender, managerAddress);
+
+    await ManagerContract.updateAggregator({ aggregator: aggregatorAddress });
   });
 
-  // import the wasm bytecode
-  let instantiate: ManagerInstantiateMsg = {
-    amount_reward: "10000",
-    owner: sender,
-    register_fee: "10000",
-    thresh_hold_for: 40,
-  };
+  it("deploy succesfully", async () => {
+    let aggregator = await ManagerContract.aggregator();
+    expect(aggregator).toEqual(aggregatorAddress);
+    expect(managerAddress).not.toBeNaN();
+    expect(aggregatorAddress).not.toBeNaN();
+  });
 
-  const { contractAddress: managerAddress } = await client.deploy(
-    sender,
-    resolve(__dirname, "../artifacts/manager_license-aarch64.wasm"),
-    instantiate,
-    "manager",
-    "auto"
-  );
-
-  let instantiate_msg: AggregatorInstantiateMsg = {
-    manager: managerAddress,
-  };
-
-  const { contractAddress: aggregatorAddress } = await client.deploy(
-    sender,
-    resolve(__dirname, "../artifacts/aggregator-aarch64.wasm"),
-    instantiate_msg,
-    "manager",
-    "auto"
-  );
-
-  console.log(aggregatorAddress, managerAddress);
-};
-
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
+  it("register host successfully", async () => { });
 });
